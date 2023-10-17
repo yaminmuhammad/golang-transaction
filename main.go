e1e68e4caa65cf2d50e911c7ede603e2fae300ea
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-10-17 14:17:02 +0700

    init commit add funcconnectDB enrollSubject

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "stanners2020"
+	dbname   = "enigmacamp"
+)
+
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+func main() {
+
+}
+
+func connectDb() *sql.DB {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully connected!")
+	}
+
+	return db
+
+}
---
+++ student/student_enrollment.go
@@ -0,0 +1,8 @@
+package entity
+
+type StudentEnrollment struct {
+	Id         int
+	Student_Id int
+	Subject    string
+	Credit     int
+}
